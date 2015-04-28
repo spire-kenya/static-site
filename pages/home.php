36176dce7c72a0d9148f54238dd5c93f7ebb1700
Stephen Kimani Njuguna <skimani41@gmail.com>
2015-04-28 13:12:44 +0300

    mixpanel tracker edits on homepage

--- pages/home.php
+++ pages/home.php
@@ -1,116 +1,116 @@
 <?php
 
-  $page_title = "Spire Kenya";
-  $meta_description = "Spire is a Nairobi-based company that provides short professional courses (3-6 weeks) that help you get hired and build a successful career.";
-  include("./inc/header.php");
+$page_title = "Spire Kenya";
+$meta_description = "Spire is a Nairobi-based company that provides short professional courses (3-6 weeks) that help you get hired and build a successful career.";
+include("./inc/header.php");
 
 ?>
 
-  <div class="splash splash-home">
+<div class="splash splash-home">
     <div class="container">
-      <div class="text-block">
-        <h2 class="editable">LATEST: New programme focused on careers in NGO launching in April</h2>
-        <a href="/career-accelerator-ngo/" class="btn btn-primary btn-lg">Find Out More</a>
-      </div>
+        <div class="text-block">
+            <h2 class="editable">LATEST: New programme focused on careers in NGO launching in April</h2>
+            <a href="/career-accelerator-ngo/" class="btn btn-primary btn-lg track" data-name="clicked find out more-top-homepage" data-properties="{page_id: 1, name: 'spire homepage'}">Find Out More</a>
+        </div>
     </div>
-  </div>
+</div>
 
-  <div class="companies">
+<div class="companies">
     <?php image_tag("companies.jpg"); ?>
-  </div>
+</div>
 
-  <div class="what-is-spire">
+<div class="what-is-spire">
     <div class="container">
-      <h1>What is Spire?</h1>
-      <p>Spire is a Kenyan company that provides short professional courses (3-6 weeks) in Nairobi.</p>
+        <h1>What is Spire?</h1>
+        <p>Spire is a Kenyan company that provides short professional courses (3-6 weeks) in Nairobi.</p>
 
-      <h4>A LEARNING PARTNER.</h4>
-      <p>Our world-class programmes are designed to give you the right training at the right time. You will receive constant feedback to help you at every step.</p>
-      
-      <h4>A WINNING APPROACH.</h4>
-      <p>Our classrooms use practical training with personal attention. The curriculum focuses on developing the skills you need with the mindset to make them work for you.</p>
+        <h4>A LEARNING PARTNER.</h4>
+        <p>Our world-class programmes are designed to give you the right training at the right time. You will receive constant feedback to help you at every step.</p>
 
-      <h4>A TEAM WITH EXPERIENCE.</h4>
-      <p>Our passionate team of coaches and curriculum designers draw on experience at companies like McKinsey and decades of experience in Kenya. Most of all, they are driven by a sense of fun and a desire to see you succeed!</p>
+        <h4>A WINNING APPROACH.</h4>
+        <p>Our classrooms use practical training with personal attention. The curriculum focuses on developing the skills you need with the mindset to make them work for you.</p>
+
+        <h4>A TEAM WITH EXPERIENCE.</h4>
+        <p>Our passionate team of coaches and curriculum designers draw on experience at companies like McKinsey and decades of experience in Kenya. Most of all, they are driven by a sense of fun and a desire to see you succeed!</p>
     </div>
-  </div>
+</div>
 
-  <div class="heading gray">
+<div class="heading gray">
     <div class="container">
-      <h2>What students and employers say about Spire:</h2>
+        <h2>What students and employers say about Spire:</h2>
     </div>
-  </div>
+</div>
 
-  <div class="testimonials container">
+<div class="testimonials container">
     <div class="row">
-      <div class="col-sm-4 testimonial">
-        <?php image_tag("student-kevin.png", false, "student-image"); ?>
-        <h4 class="student-name">KEVIN LUMUMBA</h4>
-        <strong class="student-company">Hired by KPMG</strong>
-        <p>"Spire is about much more than just helping you get a job. It's been a journey of self discovery and of self-reflection. It's one of the best investments I've ever made."</p>
-      </div>
-      <div class="col-sm-4 testimonial">
-        <?php image_tag("student-reuben.jpg", false, "student-image"); ?>
-        <h4 class="student-name">REUBEN MBINDU</h4>
-        <strong class="student-company">HR Director, Equity Bank</strong>
-        <p>"Spire gives you the skills that you don't get in university. That is the winning point of Spire in preparing students to bridge the gap between university and industry"</p>
-      </div>
-      <div class="col-sm-4 testimonial">
-        <?php image_tag("student-josemaria.jpg", false, "student-image"); ?>
-        <h4 class="student-name">JOSE MARIA NGACHA</h4>
-        <strong class="student-company">Hired by TechnoBrain</strong>
-        <p>"I am able to take on challenges without fixing my limits, and courageously stand for what is right and good for others and myself. You transformed me in a way that <span>I am still trying to put in to words.</span>"</p>
-      </div>
-    </div>
-  </div>
-
-  <div class="call-out">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-9">
-          <h3>Our next training programme starts in April</h3>
+        <div class="col-sm-4 testimonial">
+            <?php image_tag("student-kevin.png", false, "student-image"); ?>
+            <h4 class="student-name">KEVIN LUMUMBA</h4>
+            <strong class="student-company">Hired by KPMG</strong>
+            <p>"Spire is about much more than just helping you get a job. It's been a journey of self discovery and of self-reflection. It's one of the best investments I've ever made."</p>
         </div>
-        <div class="col-sm-3">
-          <a href="/career-accelerator-ngo/" class="btn btn-lg btn-white">Find Out More</a>
+        <div class="col-sm-4 testimonial">
+            <?php image_tag("student-reuben.jpg", false, "student-image"); ?>
+            <h4 class="student-name">REUBEN MBINDU</h4>
+            <strong class="student-company">HR Director, Equity Bank</strong>
+            <p>"Spire gives you the skills that you don't get in university. That is the winning point of Spire in preparing students to bridge the gap between university and industry"</p>
         </div>
-      </div>
+        <div class="col-sm-4 testimonial">
+            <?php image_tag("student-josemaria.jpg", false, "student-image"); ?>
+            <h4 class="student-name">JOSE MARIA NGACHA</h4>
+            <strong class="student-company">Hired by TechnoBrain</strong>
+            <p>"I am able to take on challenges without fixing my limits, and courageously stand for what is right and good for others and myself. You transformed me in a way that <span>I am still trying to put in to words.</span>"</p>
+        </div>
     </div>
-  </div>
+</div>
 
-  <div class="video">
+<div class="call-out">
     <div class="container">
-      <div class="video-wrapper"><iframe src="//player.vimeo.com/video/112980334" width="960" height="540" frameborder="0" title="Spire Career Accelerator" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+        <div class="row">
+            <div class="col-sm-9">
+                <h3>Our next training programme starts in April</h3>
+            </div>
+            <div class="col-sm-3">
+                <a href="/career-accelerator-ngo/" class="btn btn-lg btn-white track" data-name="clicked find out more-middle-homepage" data-properties="{page_id: 1, name: 'spire homepage'}">Find Out More</a>
+            </div>
+        </div>
     </div>
-  </div>
+</div>
 
-  <div class="stats container">
+<div class="video">
+    <div class="container">
+        <div class="video-wrapper"><iframe src="//player.vimeo.com/video/112980334" width="960" height="540" frameborder="0" title="Spire Career Accelerator" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+    </div>
+</div>
+
+<div class="stats container">
     <div class="row">
-      <div class="col-sm-4">
-        <span class="stat-number">2500</span>
-        <p class="stat-text">PEOPLE APPLY FOR EACH ACCELERATOR</p>
-      </div>
-      <div class="col-sm-4">
-        <span class="stat-number">120</span>
-        <p class="stat-text">AVAILABLE PLACES IN OUR APRIL 2015 ACCELERATOR</p>
-      </div>
-      <div class="col-sm-4">
-        <span class="stat-number">15</span>
-        <p class="stat-text">MINUTES TO APPLY</p>
-      </div>
+        <div class="col-sm-4">
+            <span class="stat-number">2500</span>
+            <p class="stat-text">PEOPLE APPLY FOR EACH ACCELERATOR</p>
+        </div>
+        <div class="col-sm-4">
+            <span class="stat-number">120</span>
+            <p class="stat-text">AVAILABLE PLACES IN OUR APRIL 2015 ACCELERATOR</p>
+        </div>
+        <div class="col-sm-4">
+            <span class="stat-number">15</span>
+            <p class="stat-text">MINUTES TO APPLY</p>
+        </div>
     </div>
-  </div>
+</div>
 
-  <div class="call-out bottom">
+<div class="call-out bottom">
     <div class="container">
-      <div class="row">
-        <div class="col-sm-9">
-          <h3>Applications are now open for our NGO-focused Career Accelerator in Nairobi</h3>
+        <div class="row">
+            <div class="col-sm-9">
+                <h3>Applications are now open for our NGO-focused Career Accelerator in Nairobi</h3>
+            </div>
+            <div class="col-sm-3">
+                <a href="/career-accelerator-ngo/" class="btn btn-lg btn-white track" data-name="clicked find out more-bottom-homepage" data-properties="{page_id: 1, name: 'spire homepage'}">Find Out More</a>
+            </div>
         </div>
-        <div class="col-sm-3">
-          <a href="/career-accelerator-ngo/" class="btn btn-lg btn-white">Find Out More</a>
-        </div>
-      </div>
     </div>
-  </div>
-
+</div>
+<script type="text/javascript">mixpanel.track("Spire Homepage")</script>
 <?php include("./inc/footer.php"); ?>
