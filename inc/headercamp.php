d615f893b7ef2aad1e9e490330ea4981a590abe5
Stephen Kimani Njuguna <skimani41@gmail.com>
2015-06-15 08:37:41 +0300

    added camp flyers pages to directory pages+accompanying images

---
+++ inc/headercamp.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <meta charset="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="icon" type="image/png" href="http://staging.spire.is/favicon.ico">
+
+    <title><?php echo $page_title; ?></title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/spire.css" rel="stylesheet">
+    <link href="/css/carousel.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:100,200,300,400,600,700,800|Open+Sans:100,200,300,400,600,700,800|Raleway:100,200,300,400,600,700,800|Roboto:100,200,300,400,600,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header big">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">
+                <?php image_tag("logo.png"); ?>
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="main-menu">
+            <ul class="nav navbar-nav navbar-right">
+                <!-- <li><a href="/partnerships/">Company partnerships</a></li>
+                 <li><a href="/schools">Education partnerships<span class="hidden-sm"></span></a></li>
+                 <li class="visible-xs"><a href="/about-the-team/">The Spire Team</a></li>
+                 <li class="visible-xs"><a href="/press-coverage/">Press Coverage</a></li>
+                 <li class="dropdown hidden-xs">
+                   <a href="/about-the-team/">The Spire Team</a>
+                   <ul class="dropdown-menu">
+                     <li><a href="/press-coverage/">Press Coverage</a></li>
+                   </ul>
+                 </li>
+                 <li><a href="http://spire.theresumator.com/" target="_blank">Careers At Spire</a></li>-->
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ pages/robocamp.php
@@ -0,0 +1,32 @@
+<?php $page_title = "Spire Kenya"; include("./inc/headercamp.php"); ?>
+<head> </head>
+<title> Welcome to Spire Schools</title>
+
+<body>
+<div class="call-out">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9">
+                <h3>Is your company ready to reach new heights?</h3>
+            </div>
+            <div class="col-sm-3">
+                <a href="/partnerships/" class="btn btn-lg btn-white">Find Out More</a>
+            </div>
+        </div>
+    </div>
+</div>
+<p align = "center"><img src = "images/Robocamp.png" width = "1000" height = "1500">
+</p>
+<div class="call-out">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9">
+                <h3>Is your company ready to reach new heights?</h3>
+            </div>
+            <div class="col-sm-3">
+                <a href="/partnerships/" class="btn btn-lg btn-white">Find Out More</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
