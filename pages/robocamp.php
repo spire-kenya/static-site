1286b6e3698022597cefb31d340d20aac6279328
Stephen Kimani Njuguna <skimani41@gmail.com>
2015-06-15 09:25:13 +0300

    changed copy on camp flyer pages

--- pages/robocamp.php
+++ pages/robocamp.php
@@ -7,15 +7,15 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-9">
-                <h3>We build education programs that work</h3>
+                <h3>Is your child ready to learn this August?</h3>
             </div>
             <div class="col-sm-3">
-                <a href="http://goo.gl/forms/kPpsxamSLe" class="btn btn-lg btn-white">Find Out More</a>
+                <a href="http://goo.gl/forms/kPpsxamSLe" class="btn btn-lg btn-white">Join Our Camp</a>
             </div>
         </div>
     </div>
 </div>
-<p align = "center"><img src = "images/Robocamp.png" width = "1000" height = "1500">
+<p align = "center"><img src = "images/Robocamp.png" width = "900" height = "1350">
 </p>
 <div class="call-out">
     <div class="container">
